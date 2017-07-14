30afa473f85c62f691d7f28d989c4c4a307504b0
qinghaitvxq <598616202@qq.com>
2017-07-14 16:58:34 +0800

    webpack config

--- src/auto_sel_cbx/index.js
+++ src/auto_sel_cbx/index.js
@@ -1,7 +1,64 @@
 /**
  * Created by caipf on 2017/7/13.
  */
-console.log('this is ');
-console.log('hellosdsss');
-console.log('heelldd');
+console.log("this is auto_sel_cbx js");
 
+// (function () {
+//
+// })(jQuery);
+// let ctlconfigdata=[
+//     {
+//         "roomTp":"1",
+//         "configData":{
+//             "bedWidth":"1.5",
+//             "hasWin":"0",
+//             "bedNum":"2"
+//         }
+//     },{
+//         "roomTp":"2",
+//         "configData":{
+//             "bedWidth":"1.8",
+//             "hasWin":"1",
+//             "bedNum":"2"
+//         }
+//     },{
+//         "roomTp":"3",
+//         "configData":{
+//             "bedWidth":"2",
+//             "hasWin":"0",
+//             "bedNum":"3"
+//         }
+//     }, {
+//         "roomTp":"4",
+//         "configData":{
+//             "bedWidth":"1.8",
+//             "hasWin":"0",
+//             "bedNum":"2"
+//         }
+//     },{
+//         "roomTp":"5",
+//         "configData":{
+//             "bedWidth":"1.5",
+//             "hasWin":"0",
+//             "bedNum":"2"
+//         }
+//     }
+// ];
+// $(".select_ctl").change(function () {
+//     //clear all options
+//     $(".autofill_sel_cbx").find(":checked").attr("checked",false);
+//     let rt=this.value;
+//     for(let i=0;i<ctlconfigdata.length;i++){
+//         let item=ctlconfigdata[i];
+//         if(rt==item.roomTp){
+//             let bedWidth=item.configData.bedWidth,
+//                 hasWin=item.configData.hasWin,
+//                 bedNum=item.configData.bedNum;
+//             $(".autofill_sel_cbx").find(`.bedWidth`).find(`input[value='${bedWidth}']`).attr("checked",true);
+//             $(".autofill_sel_cbx").find(`.hasWin`).find(`input[value='${hasWin}']`).attr("checked",true);
+//             $(".autofill_sel_cbx").find(`.bedNum`).find(`input[value='${bedNum}']`).attr("checked",true);
+//
+//             break;
+//         }
+//     }
+// });
---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by caipf on 2017/7/14.
+ */
+var auto_sel_cbx=require('./auto_sel_cbx/index');
+console.log('this is src index.js');
